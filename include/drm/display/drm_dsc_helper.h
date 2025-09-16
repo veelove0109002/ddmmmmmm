5916d6d5a414453b82f25913f93fc42797db2db7
veelove0109008 <veelove0109008@gmail.com>
2025-09-16 18:33:09 +0800

    新hddddd

--- include/drm/display/drm_dsc_helper.h
+++ include/drm/display/drm_dsc_helper.h
@@ -1,5 +1,13 @@
 #include <linux/version.h>
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 15, 0)
 #include_next <drm/display/drm_dsc_helper.h>
+#else
+/* For older kernels, include the legacy drm_dsc.h if available */
+#ifdef CONFIG_DRM_DSC
+#include <drm/drm_dsc.h>
+#endif
+#endif
 
 #ifndef _BACKPORT_DRM_DISPLAY_DRM_DSC_HELPER_H
 #define _BACKPORT_DRM_DISPLAY_DRM_DSC_HELPER_H
